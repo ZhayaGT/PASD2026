4c7fcc8563918039dc04a3b6f442f35e405be7bb
ZhayaGT <Jonathanemmanuel0107@gmail.com>
2026-02-22 14:44:12 +0700

    Minngu2_Percobaan2

---
+++ Minggu_2/Script/MahasiswaMain16.java
@@ -0,0 +1,16 @@
+package Minggu_2.Script;
+
+public class MahasiswaMain16 {
+    public static void main(String[] args) {
+        Mahasiswa16 mhs1 = new Mahasiswa16();
+        mhs1.nama16 = "Muhammad Ali Farhan";
+        mhs1.nim16 = "2241720171";
+        mhs1.kelas16 = "SI 2J";
+        mhs1.ipk16 = 3.55;
+
+        mhs1.tampilkanInformasi();
+        mhs1.ubahKelas("SI 2K");
+        mhs1.updateIPK(3.60);
+        mhs1.tampilkanInformasi();
+    }
+}
