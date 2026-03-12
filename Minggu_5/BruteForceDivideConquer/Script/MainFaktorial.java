75bb8897a2c5bd22c797e99767d453273b5cb926
ZhayaGT <jonathanemmanuel0107@gmail.com>
2026-03-12 10:55:58 +0700

    Minggu5_Percobaan2

--- Minggu_5/BruteForceDivideConquer/Script/MainFaktorial.java
+++ Minggu_5/BruteForceDivideConquer/Script/MainFaktorial.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class MainFaktorial {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner jonathan = new Scanner(System.in);
         System.out.print("Masukan nilai: ");
-        int nilai = input.nextInt();
+        int nilai = jonathan.nextInt();
 
         Faktorial fk = new Faktorial();
         System.out.println("Nilai faktorial " + nilai + " menggunakan BF: " + fk.faktorialBF(nilai));
