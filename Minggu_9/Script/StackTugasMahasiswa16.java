62338ec2a4e26695128388c19ca486471dede761
ZhayaGT <jonathanemmanuel0107@gmail.com>
2026-04-24 09:01:22 +0700

    Minggu9_Percobaan1

---
+++ Minggu_9/Script/StackTugasMahasiswa16.java
@@ -0,0 +1,65 @@
+package Minggu_9.Script;
+
+public class StackTugasMahasiswa16 {
+    Mahasiswa16[] stack;
+    int top;
+    int size;
+
+    StackTugasMahasiswa16(int size) {
+        this.size = size;
+        stack = new Mahasiswa16[size];
+        top = -1;
+    }
+
+    boolean isFull() {
+        if (top == size -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void push(Mahasiswa16 mhs) {
+        if (!isFull()) {
+            top++;
+            stack[top] = mhs;
+        } else {
+            System.out.println("Stack penuh! Tidak bisa menambahkan tugas lagi. ");
+        }
+    }
+
+    Mahasiswa16 pop() {
+        if (!isEmpty()) {
+            Mahasiswa16 m = stack[top];
+            top--;
+            return m;
+        } else {
+            System.out.println("Stack kosong! tidak ada tugas untuk dinilai");
+            return null;
+        }
+    }
+
+    Mahasiswa16 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else {
+            System.out.println("Stack kosong! Tidak ada tugas yang dikumpulkan");
+            return null;
+        }
+    }
+
+    void print() {
+        for (int i = top; i >= 0; i--) {
+            System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+        }
+        System.out.println("");
+    }
+}
